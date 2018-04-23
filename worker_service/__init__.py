f2fa22baeb1fe788d761002b7760adceb51b6c8b
valavanca <valavanca@gmail.com>
2018-04-23 23:10:10 +0200

    CORS request to service

--- worker_service/__init__.py
+++ worker_service/__init__.py
@@ -1,11 +1,13 @@
 import os
 
 from flask import Flask 
+from flask_cors import CORS
 
 def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+    CORS(app) # !!! 
 
     # register blueprints
     from api.router import service_blueprint
