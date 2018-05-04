585e0a004e119b47e829a06f0c30b094bdadcf00
valavanca <valavanca@gmail.com>
2018-05-04 17:11:01 +0200

    real worker init

--- worker/worker.py
+++ worker/worker.py
@@ -17,3 +17,10 @@ def random_2(param):
     data = Splitter("csv_data/"+param['ws_file'])
     time.sleep(randint(1, 5))
     return data.search(str(param['frequency']), str(param['threads']))
+
+def random_real(param):
+    data = Splitter("csv_data/"+param['ws_file'])
+    result = data.search(str(param['frequency']), str(param['threads']))
+    if result["TIM"]: time.sleep(result["TIM"]/10000) 
+    else: time.sleep(2)
+    return result
