56bbd5af350cbb1f79306c53cdb41e5b855268ef
Ievgeniia Svetsynska <ievgeniia.svetsynska@tu-dresden.de>
2018-08-27 20:44:30 +0200

    test

--- main-node/model/model_selection.py
+++ main-node/model/model_selection.py
@@ -11,8 +11,6 @@ def get_model(model_creation_config, log_file_name, features, labels):
     :return: Instantiated object of prediction model.
     """
 
-
-
     if model_creation_config["ModelType"] == "regression":
         return RegressionSweetSpot(log_file_name=log_file_name,
                                    test_size=model_creation_config["ModelTestSize"],
