01d4a00269723e929767165951b8c2ff0252e145
Ievgeniia <ievgeniia@edr09820.dip.tu-dresden.de>
2018-08-27 14:47:48 +0200

    Added global variables

--- main-node/tests/test_repeater/test_repeater_selection.py
+++ main-node/tests/test_repeater/test_repeater_selection.py
@@ -3,9 +3,8 @@ from repeater.default_repeater import DefaultRepeater
 from repeater.student_repeater import StudentRepeater
 import pytest
 
-WS = "WS"
-
-experiments = {
+WS = "should be WS"
+EXPERIMENTS = {
         "TaskName": "energy_consumption",
         "FileToRead": "Radix-500mio.csv",
         "ResultStructure": ["frequency", "threads", "energy"],
@@ -14,20 +13,22 @@ experiments = {
         "MaxRepeatsOfExperiment": 4
     }
 
+
 def test_default_repeater():
     repeater_type = "default"
-    assert isinstance(get_repeater(repeater_type, WS, experiments), DefaultRepeater)
+    assert isinstance(get_repeater(repeater_type, WS, EXPERIMENTS), DefaultRepeater)
+
 
 def test_student_repeater():
     repeater_type = "student_deviation"
-    assert isinstance(get_repeater(repeater_type, WS, experiments), StudentRepeater)
+    assert isinstance(get_repeater(repeater_type, WS, EXPERIMENTS), StudentRepeater)
 
 
-def test_KeyError():
-    repeater_type = "something"
+def test_key_error():
+    repeater_type = "should be repeater type"
     with pytest.raises(KeyError):
-        get_repeater(repeater_type, WS, experiments)
+        get_repeater(repeater_type, WS, EXPERIMENTS)
 
     repeater_type = 123
     with pytest.raises(KeyError):
-        get_repeater(repeater_type, WS, experiments)
+        get_repeater(repeater_type, WS, EXPERIMENTS)
--- main-node/tests/test_repeater/test_student_repeater.py
+++ main-node/tests/test_repeater/test_student_repeater.py
@@ -2,41 +2,34 @@ from repeater.student_repeater import StudentRepeater
 from repeater.default_repeater import DefaultRepeater
 from repeater.history import History
 
+
+WS = "should be WS"
+EXPERIMENTS = {
+    "TaskName": "energy_consumption",
+    "FileToRead": "Radix-500mio.csv",
+    "ResultStructure": ["frequency", "threads", "energy"],
+    "ResultDataTypes": ["float", "int", "float"],
+    "RepeaterDecisionFunction": "student_deviation",
+    "MaxRepeatsOfExperiment": 4
+}
+
 def test_default_student_repeater():
-    WS = "WS"
-    experiments = {
-        "TaskName": "energy_consumption",
-        "FileToRead": "Radix-500mio.csv",
-        "ResultStructure": ["frequency", "threads", "energy"],
-        "ResultDataTypes": ["float", "int", "float"],
-        "RepeaterDecisionFunction": "student_deviation",
-        "MaxRepeatsOfExperiment": 4
-    }
-    student_repeater = StudentRepeater(WS, experiments)
+    student_repeater = StudentRepeater(WS, EXPERIMENTS)
     assert isinstance(student_repeater.default_repeater, DefaultRepeater)
 
 def test_decision_function():
-    WS = "WS"
-    experiments = {
-        "TaskName": "energy_consumption",
-        "FileToRead": "Radix-500mio.csv",
-        "ResultStructure": ["frequency", "threads", "energy"],
-        "ResultDataTypes": ["float", "int", "float"],
-        "RepeaterDecisionFunction": "student_deviation",
-        "MaxRepeatsOfExperiment": 4
-    }
-    student_repeater = StudentRepeater(WS, experiments)
+
+    student_repeater = StudentRepeater(WS, EXPERIMENTS)
     test_history = History()
     point = (2900, 32)
     result = student_repeater.decision_function(test_history, point)
-    # if test_history is empty
-    assert result is False
+    assert result is False  # if test_history is empty
 
     values = [1, 50]
     test_history.put(point, values[0])
     result = student_repeater.decision_function(test_history, point)
-    #if test_history has one element
-    assert result is False
+    assert result is False  # if test_history has one element
+
 
     #TODO - problem with WSClient
     # test_history.put(point, values[1])
