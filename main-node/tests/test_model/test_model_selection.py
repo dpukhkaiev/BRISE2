f33786abeb51d40cdd2c2b6765eb363588f11ff3
Ievgeniia <ievgeniia@edr09820.dip.tu-dresden.de>
2018-08-27 14:20:23 +0200

    Added global variables

--- main-node/tests/test_model/test_model_selection.py
+++ main-node/tests/test_model/test_model_selection.py
@@ -3,44 +3,33 @@ from model.regression_sweet_spot import RegressionSweetSpot
 import pytest
 
 
-# fail if there is no "ModelTestSize"
-def test_get_model_without_ModelTestSize():
-    model_creation_config = {
-        # "ModelTestSize": 0.9,
+MODEL_CREATION_CONFIG = {
+        "ModelTestSize": 0.9,
         "MinimumAccuracy": 0.85,
         "ModelType": "regression",
         "FeaturesLabelsStructure": ["feature", "feature", "label"]
     }
-    log_file_name = "123"
-    features = []
-    labels = []
+LOG_FILE_NAME = "should_be_log_file_name"
+FEATURES = []
+LABELS = []
 
-    with pytest.raises(KeyError):
-        get_model(model_creation_config, log_file_name, features, labels)
+
+def test_get_model_without_model_test_size():
+    model_creation_config = {
+        "MinimumAccuracy": 0.85,
+        "ModelType": "regression",
+        "FeaturesLabelsStructure": ["feature", "feature", "label"]
+    }
+    with pytest.raises(KeyError):  # fail if there is no "ModelTestSize"
+        get_model(model_creation_config, LOG_FILE_NAME, FEATURES, LABELS)
+
 
 def test_get_model_regression():
-    model_creation_config = {
-        "ModelTestSize": 0.9,
-        "MinimumAccuracy": 0.85,
-        "ModelType": "regression",
-        "FeaturesLabelsStructure": ["feature", "feature", "label"]
-    }
-    log_file_name = "123"
-    features = []
-    labels = []
-    result = get_model(model_creation_config, log_file_name, features, labels)
+    result = get_model(MODEL_CREATION_CONFIG, LOG_FILE_NAME, FEATURES, LABELS)
     assert isinstance(result, RegressionSweetSpot)
 
-def test_KeyError():
-    log_file_name = "123"
-    features = []
-    labels = []
-    model_creation_config2 = {
-        "ModelTestSize": 0.9,
-        "MinimumAccuracy": 0.85,
-        "ModelType": "not regression",
-        "FeaturesLabelsStructure": ["feature", "feature", "label"]
-    }
 
+def test_key_error():
+    MODEL_CREATION_CONFIG["ModelType"] = "not regression"
     with pytest.raises(KeyError):
-        get_model(model_creation_config2, log_file_name, features, labels)
+        get_model(MODEL_CREATION_CONFIG, LOG_FILE_NAME, FEATURES, LABELS)
