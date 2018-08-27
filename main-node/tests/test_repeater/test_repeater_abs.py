2e5427d0ad39731f9e923021db9cb2e8f7273a50
Ievgeniia <ievgeniia@edr09820.dip.tu-dresden.de>
2018-08-27 14:43:14 +0200

    'test_repeater_abs' - added global variables, fixed according to 'Repeater' class

--- main-node/tests/test_repeater/test_repeater_abs.py
+++ main-node/tests/test_repeater/test_repeater_abs.py
@@ -1,29 +1,32 @@
 from repeater.repeater_abs import Repeater
 from repeater.history import History
 
-experiments = {
+EXPERIMENTS = {
         "TaskName": "energy_consumption",
         "FileToRead": "Radix-500mio.csv",
         "ResultStructure": ["frequency", "threads", "energy"],
         "ResultDataTypes": ["float", "int", "float"],
         "RepeaterDecisionFunction": "student_deviation",
         "MaxRepeatsOfExperiment": 4
-    }
+}
+WS = "should be WS"
 
-class My_Repeater(Repeater):
+
+class MyRepeater(Repeater):
     def decision_function(self, history, point, iterations=10, **configuration):
         all_experiments = history.get(point)
         if len(all_experiments) < iterations:
             return False
 
-def test_default_Repeater():
-    WSClient_exp = "WS"
-    def_Repeater = My_Repeater(WSClient_exp, experiments)
-    assert def_Repeater.WSClient == WSClient_exp
-    assert isinstance(def_Repeater.history, History)
-    assert def_Repeater.current_measurement == {}
-    assert def_Repeater.current_measurement_finished is False
-    assert def_Repeater.performed_measurements == 0
+
+def test_default_repeater():
+    def_repeater = MyRepeater(WS, EXPERIMENTS)
+    assert def_repeater.WSClient == WS
+    assert isinstance(def_repeater.history, History)
+    assert def_repeater.current_measurement == {}
+    assert def_repeater.current_measurement_finished is False
+    assert def_repeater.performed_measurements == 0
+    assert def_repeater.max_repeats_of_experiment == EXPERIMENTS["MaxRepeatsOfExperiment"]
 
 # TODO - ??? test abstract method "decision_function"
 
