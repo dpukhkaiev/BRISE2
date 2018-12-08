dd865ad9ad45ba855cc32c0c460ba88fd944fb5a
unknown <valavanca@gmail.com>
2018-12-08 21:10:32 +0100

    schema validation with the stop condition

--- main-node/Resources/validator.py
+++ main-node/Resources/validator.py
@@ -1,4 +1,4 @@
-from jsonschema import validate
+from jsonschema import validate, RefResolver, Draft4Validator
 import json
 
 # Calibrate directory //getcwd
@@ -11,16 +11,20 @@ chdir(join(abspath('.'), 'main-node'))
 from tools.file_system_io import load_json_file
 
 
-def is_experiment_description_valid(path_to_shema="resources/task.json"):
+def is_experiment_description_valid(schema_path="resources/schema/task.schema.json", 
+                                    file_path="resources/task.json"):
     """
     Validation function for json files.
+    :schema_path: string. The template schema for validation.
     :file_path: string. The file for validation.
     :return: boolean. Is file valid
     """
-    data = load_json_file(path_to_shema)
-    schema = load_json_file("resources/schema/task.schema.json") # validation schema
+    data = load_json_file(file_path)
+    schema = load_json_file(schema_path)
+    
+    resolver = RefResolver('file:///' + abspath('.').replace("\\", "/") + '/', schema)
 
-    return validate(data, schema) == None
+    return Draft4Validator(schema, resolver=resolver).validate(data) == None
 
 
 if __name__ == "__main__":
