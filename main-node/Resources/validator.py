49bf609e2556fa6eddae708fcc900cdb287669dd
yevheniisemendiak <semendyak@gmail.com>
2019-01-23 10:57:44 +0100

    Fix typo in default parameters

--- main-node/Resources/validator.py
+++ main-node/Resources/validator.py
@@ -13,8 +13,8 @@ from jsonschema import validate, RefResolver, Draft4Validator
 # print("----------------")
 from tools.file_system_io import load_json_file
 
-def is_experiment_description_valid(schema_path="resources/schema/task.schema.json", 
-                                    file_path="resources/task.json"):
+def is_experiment_description_valid(schema_path="Resources/schema/task.schema.json", 
+                                    file_path="Resources/task.json"):
     """
     Validation function for json files.
     :schema_path: string. The template schema for validation.
