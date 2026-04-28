d9031b6b045f219bdd87fb3c067c8d51254b5dac
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2026-04-28 14:06:29 +0200

    skip test test

--- main_node/configuration_selection/tests/test_configuration_selection.py
+++ main_node/configuration_selection/tests/test_configuration_selection.py
@@ -649,6 +649,7 @@ class TestConfigurationSelection:
         assert len(cs.predictor.mapping_region_model[temp_region].mapping_surrogate_objective) == 1  # SO
         experiment.dump("Results")
 
+    @pytest.mark.skip(reason="no way of currently testing this")
     def test_14(self, get_experiment, get_workers, get_configurations_2_float):
         """
         ['2 float', 'flat', 'so', 'mo.none', 'lr', 'surr.vt.none', 'surr.ct.none',
