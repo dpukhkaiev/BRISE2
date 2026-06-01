d1caeef12fd6b43f2a02077670f3b09e5a68d4ae
Jonas <jonas.noethel@mailbox.tu-dresden.de>
2026-06-01 10:42:45 +0200

    Added benchmark experiments

--- benchmark/benchmark_runner.py
+++ benchmark/benchmark_runner.py
@@ -563,12 +563,12 @@ class BRISEBenchmarkRunner:
         }
         # test case with 2 float parameters
         self._base_experiment_description, self._base_search_space = \
-            load_experiment_setup("./Resources/test/test_cases_product_configurations/test_case_0.json")
+            load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_0.json")
         experiment_description = self.base_experiment_description
         experiment_description.update(deepcopy(time_based_sc_skeleton))
         experiment_description.update(deepcopy(flat_2float_model_skeleton))
         self.execute_experiment(experiment_description, number_of_repetitions=1)
-
+        return self.counter
         # test case with float nom parameters
         self._base_experiment_description, self._base_search_space = \
             load_experiment_setup("./Resources/test/test_cases_product_configurations/test_case_4.json")
@@ -603,16 +603,7 @@ class BRISEBenchmarkRunner:
     
     @_benchmarkable
     def reconf_benchmark(self):
-        #TODO: Create experiments to benchmark
-        """IDEAS:
-        - Simple changes
-        - Multi Surrogate/optimizers
-        - Replace Model
-        - Replace Predictor
-
-        - Get every test experiment and replace the stop condition, candidateSelector and sampling strategy
-        """
-        self._experiment_timeout = 2 * 60
+        self._experiment_timeout = 1 * 60
 
         reconf_sampling_and_candidate = {
             "Reconfiguration": {
@@ -631,12 +622,343 @@ class BRISEBenchmarkRunner:
             }
         }
 
-        # Change sampling strategy and candidate selector during the experiment for all test cases
-        for exp_num in range(15):
-            self._base_experiment_description, self._base_search_space = \
-                load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_" + str(exp_num) + ".json")
-            experiment_description = self.base_experiment_description
-            experiment_description.update(deepcopy(reconf_sampling_and_candidate))
+        reconf_stop_condition = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 5,
+                    "performAmount": 1,
+                    "vp": "StopCondition",
+                    "description": {"Instance": {
+                        "TimeBasedSC": {
+                            "Parameters": {
+                                    "MaxRunTime": 10,
+                                    "TimeUnit": "seconds"
+                                },
+                                "Type": "time_based",
+                                "Name": "t"
+                            }
+                        },
+                        "StopConditionTriggerLogic": {
+                            "Expression": "t",
+                            "InspectionParameters": {
+                                "RepetitionPeriod": 1,
+                                "TimeUnit": "seconds"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        reconf_optimizer = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 5,
+                    "performAmount": 1,
+                    "vp": "Optimizer",
+                    "description": {"Instance": { "RandomSearch": {
+                        "SamplingSize": 96,
+                        "MultiObjective": False,
+                        "Type": "random_search"
+                    }}}
+                }
+            }
+        }
+
+        reconf_validator = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 5,
+                    "performAmount": 1,
+                    "vp": "Validator",
+                    "description": {
+                        "ExternalValidator": {
+                            "QualityValidator": {
+                                "Split": {
+                                    "HoldOut": {
+                                        "TrainingSet": 0.6
+                                    }
+                                },
+                                "QualityThreshold": 0.3,
+                                "Type": "quality_validator"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        reconf_surrogate = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 5,
+                    "performAmount": 1,
+                    "vp": "Surrogate",
+                    "description":{
+                        "Instance": {
+                            "LinearRegression": {
+                                "MultiObjective": False,
+                                "Type": "sklearn_model_wrapper",
+                                "Class": "sklearn.linear_model.LinearRegression"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        reconf_rep_manager = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 5,
+                    "performAmount": 1,
+                    "vp": "RepetitionManager",
+                    "description":{
+                        "MaxFailedTasksPerConfiguration": 2,
+                        "Instance": {
+                            "QuantityBased": {
+                                "MaxTasksPerConfiguration": 2,
+                                "Type": "quantity_based"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        reconf_transfer_learning = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 2,
+                    "performAmount": 1,
+                    "vp": "TransferLearning",
+                    "description": {}
+                }
+            }
+        }
+
+        reconf_single_optimizer = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 2,
+                    "performAmount": 1,
+                    "vp": "Optimizer_0",
+                    "identifiers": ["Model_1"],
+                    "description": {"Instance": { "RandomSearch": {
+                        "SamplingSize": 500,
+                        "MultiObjective": True,
+                        "Type": "random_search"
+                    }}}
+                }
+            }
+        }
+
+        reconf_single_surrogate = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 2,
+                    "performAmount": 1,
+                    "vp": "Surrogate_0",
+                    "identifiers": ["Model_1"],
+                    "description": {
+                        "Instance": {
+                            "ModelMock": {
+                                "MultiObjective": True,
+                                "Type": "model_mock"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        reconf_model = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 2,
+                    "performAmount": 1,
+                    "vp": "TransferLearning",
+                    "description": {
+                        "MultiObjectiveHandling": {
+                                "SurrogateType": {
+                                    "Scalar": {}
+                                }
+                            },
+                            "Surrogate": {
+                                "ValueTransformers": {
+                                    "ValueScalarizator": {
+                                        "WeightedSum": {
+                                            "Weights": [1, 2],
+                                            "Type": "weighted_sum"
+                                        }
+                                    }
+                                },
+                                "Instance": {
+                                    "MultiArmedBandit": {
+                                        "MultiObjective": False,
+                                        "CType": "std",
+                                        "CFloat": 1.0,
+                                        "Parameters": {
+                                            "c": "std"
+                                        },
+                                        "Type": "multi_armed_bandit"
+                                    }
+                                }
+                            },
+                            "Optimizer": {
+                                "Instance": {
+                                    "RandomSearch": {
+                                        "SamplingSize": 500,
+                                        "MultiObjective": True,
+                                        "Type": "random_search"
+                                    }
+                                }
+                            },
+                            "Validator": {
+                                "ExternalValidator": {
+                                    "MockValidator": {
+                                        "Type": "mock_validator"
+                                    }
+                                }
+                            },
+                            "CandidateSelector": {
+                                "BestMultiPointProposal": {
+                                    "NumberOfPoints": 1,
+                                    "Type": "best_multi_point"
+                                }
+                            }
+                    }
+                }
+            }
+        }
+
+        reconf_predictor = {
+            "Reconfiguration": {
+                  "AfterXConfigurations": {
+                    "amount": 2,
+                    "performAmount": 1,
+                    "vp": "Predictor",
+                    "description": {"WindowSize": 1,
+                        "Model": {
+                            "Surrogate": {
+                                "ConfigurationTransformers": {
+                                    "FloatTransformer": {
+                                        "SklearnFloatMinMaxScaler": {
+                                            "Type": "sklearn_float_transformer",
+                                            "Class": "sklearn.MinMaxScaler"
+                                        }
+                                    }
+                                },
+                                "Instance": {
+                                    "TreeParzenEstimator": {
+                                        "MultiObjective": False,
+                                        "Parameters": {
+                                            "top_n_percent": 30,
+                                            "random_fraction": 0.1,
+                                            "bandwidth_factor": 3.0,
+                                            "min_bandwidth": 0.001
+                                        },
+                                        "Type": "tree_parzen_estimator"
+                                    }
+                                }
+                            },
+                            "Optimizer": {
+                                "ConfigurationTransformers": {
+                                    "FloatTransformer": {
+                                        "SklearnFloatMinMaxScaler": {
+                                            "Type": "sklearn_float_transformer",
+                                            "Class": "sklearn.MinMaxScaler"
+                                        }
+                                    }
+                                },
+                                "ValueTransformers": {
+                                    "AcquisitionFunction": {
+                                        "TPE_EI": {
+                                            "Type": "tpe_ei"
+                                        }
+                                    }
+                                },
+                                "Instance": {
+                                    "MOEA": {
+                                        "Generations": 10,
+                                        "PopulationSize": 100,
+                                        "Algorithms": {
+                                            "GACO": {
+                                                "MultiObjective": False
+                                            }
+                                        },
+                                        "Type": "moea"
+                                    }
+                                }
+                            },
+                            "Validator": {
+                                "ExternalValidator": {
+                                    "MockValidator": {
+                                        "Type": "mock_validator"
+                                    }
+                                }
+                            },
+                            "CandidateSelector": {
+                                "RandomMultiPointProposal": {
+                                    "NumberOfPoints": 1,
+                                    "Type": "random_multi_point"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        self._base_experiment_description, self._base_search_space = \
+            load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_dynamic.json")
+        experiment_description = self.base_experiment_description
+        #experiment_description.update(deepcopy(reconf_sampling_and_candidate))
+        self.execute_experiment(experiment_description, number_of_repetitions=1)
+        
+        return self.counter
+    
+        # Change optimizer, validator, surrogate, etc. during experiment
+        self._base_experiment_description, self._base_search_space = \
+            load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_dynamic.json")
+        experiment_description = self.base_experiment_description
+        
+        for reconf_skeleton in [reconf_optimizer, reconf_validator, reconf_surrogate, reconf_rep_manager]:
+            experiment_description.update(deepcopy(reconf_skeleton))
+            self.execute_experiment(experiment_description, number_of_repetitions=1)
+
+        # Change sampling strategy, candidate selector and stop condition during the experiment for all test cases
+        for reconf_skeleton in [reconf_sampling_and_candidate, reconf_stop_condition]:
+            for exp_num in range(1):#15
+                self._base_experiment_description, self._base_search_space = \
+                    load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_" + str(exp_num) + ".json")
+                experiment_description = self.base_experiment_description
+                experiment_description.update(deepcopy(reconf_sampling_and_candidate))
+                self.execute_experiment(experiment_description, number_of_repetitions=1)
+
+        # Turn of transfer learning, and predictor
+        self._base_experiment_description, self._base_search_space = \
+            load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_0.json")
+        experiment_description = self.base_experiment_description
+
+        for reconf_skeleton in [reconf_transfer_learning, reconf_predictor]:
+            experiment_description.update(deepcopy(reconf_skeleton))
+            self.execute_experiment(experiment_description, number_of_repetitions=1)
+
+        # Change single optimizer
+        self._base_experiment_description, self._base_search_space = \
+            load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_3.json")
+        experiment_description = self.base_experiment_description
+        experiment_description.update(deepcopy(reconf_single_optimizer))
+        self.execute_experiment(experiment_description, number_of_repetitions=1)
+
+        # Change entire model and single surrogate
+        self._base_experiment_description, self._base_search_space = \
+            load_experiment_setup("./Resources/tests/test_cases_product_configurations/test_case_8.json")
+        experiment_description = self.base_experiment_description
+
+        for reconf_skeleton in [reconf_model, reconf_single_surrogate]:
+            experiment_description.update(deepcopy(reconf_skeleton))
             self.execute_experiment(experiment_description, number_of_repetitions=1)
 
         return self.counter
